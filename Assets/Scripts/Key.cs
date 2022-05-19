4167d60230e7e7f2b0150afd42e0f3cd494933f1
Harles-A <harles.asu@gmail.com>
2022-05-19 11:04:31 +0300

    Added tutorial npcs

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    [SerializeField] private KeyType keyType;
+    public enum KeyType
+    {
+        MainDoor,
+        SecretDoor,
+        TreasureChest
+    }
+
+    public KeyType GetKeyType()
+    {
+        return keyType;
+    }
+
+
+}
